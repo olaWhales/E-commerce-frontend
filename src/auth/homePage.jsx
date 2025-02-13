48e0b3c1f674c3ba54e86c25aeea9f49e46b5e68
olawhales <ajaditaoreed@gmail.com>
2025-02-13 19:52:51 +0100

    refactory the nav_bar

--- src/auth/homePage.jsx
+++ src/auth/homePage.jsx
@@ -34,62 +34,68 @@ const HomePage = () => {
   return (
     <div>
       <div className={style.navBar_body}>
-        <div>
-          <h1>
+        <div className={style.company_name}>
+          <h1 className={style.name}>
             <i>
-              @ALT<sub>market</sub>
+              @whales<sub>market</sub>
             </i>
           </h1>
         </div>
 
-        {/* Market Image */}
-        {/* <div className={style.market}>
-                <img src={marketpicture} alt="Market" width={20} height={15} />
-            </div> */}
-
-        {/* Search Bar */}
         <div className={style.search_bar}>
-          <div className={style.navbardiv}>
+          <div className={style.navbar_search_div}>
             <input
-              className={style.search_input_box}
+              className={style.nav_bar_search_input}
               type="text"
-              placeholder="Search"
+              placeholder="Search product and cartegory"
             />
           </div>
-          <div className={style.button}>
+          <div className={style.search_bar_button}>
             <button className={style.search}>Search</button>
           </div>
         </div>
 
-        {/* Cart Logo */}
-        <div className={style.cartlogo}>
-          <img src={cartlogo} alt="Cart Logo" width={150} height={33} />
-          <span className={style.cartCount}>{cartCount}</span>
+        <div className={style.login_logout_and_cartDiv}>
+          <div className={style.cartlogo}>
+            <img src={cartlogo} alt="Cart Logo" width={150} height={33} />
+            <span className={style.cartCount}>{cartCount}</span>
+          </div>
+
+          {/* <div className={style.navButton}> */}
+          <div>
+            <Customizedbutton
+              style={style.Loginbutton}
+              type="submit"
+              textContent="Login"
+              onClick={handleNavigate}
+              route="/"
+              // className={style}
+            />
+          </div>
+
+          <div>
+            <Customizedbutton
+              style={style.Logoutbutton}
+              type="submit"
+              textContent="Logout"
+              // onClick={handleNavigateToHomePage}
+              // route="/homePage"
+              // className={style}
+            />
+          </div>
+          {/* </div> */}
         </div>
+      </div>
 
-        <div className={style.navButton}>
-          <Customizedbutton
-            style={style.Loginbutton}
-            type="submit"
-            textContent="Login"
-            onClick={handleNavigate}
-            route="/"
-            // className={style}
-          />
-
-          <Customizedbutton
-            style={style.Logbutton}
-            type="submit"
-            textContent="Logout"
-            // onClick={handleNavigateToHomePage}
-            // route="/homePage"
-            // className={style}
-          />
+      <div className={style.second_navbar_ProductFlyer}>
+        <div className={style.marquee}>
+          <h1 className={style.modern}>
+            <i>WE HAVE MODERN DESIGN WITH HIGH QUALITY MATERIALS,</i>{" "}
+          </h1>
+          <h1 className={style.modern2}>
+            <i>IT'S ALL IN AFFORDABLE PRICE</i>
+          </h1>
         </div>
-
-        {/* <div className={style.logout_button}>
-          <button className={style.logout}>Logout</button>
-        </div> */}
       </div>
 
       <div className={style.firstDivDisplay}>
@@ -110,18 +116,6 @@ const HomePage = () => {
         </div>
       </div>
 
-      <div className={style.second_navbar_ProductFlyer}>
-        <div className={style.marquee}>
-          <h1 className={style.modern}>
-            <i>WE HAVE MODERN DESIGN WITH HIGH QUALITY MATERIALS,</i>{" "}
-          </h1>
-          <h1 className={style.modern2}>
-            <i>IT'S ALL IN AFFORDABLE PRICE</i>
-          </h1>
-        </div>
-      </div>
-
-      {/* Cart Section */}
       <div>
         <button onClick={handleAddToCart}>Add Item</button>
         <button onClick={handleRemoveFromCart}>Remove Item</button>
