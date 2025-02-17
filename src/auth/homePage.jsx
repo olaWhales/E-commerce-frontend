64956fb1e61c4aa2d675cd7c15d3e59ced1b52ed
olawhales <ajaditaoreed@gmail.com>
2025-02-17 22:24:10 +0100

    refactory images uploading

--- src/auth/homePage.jsx
+++ src/auth/homePage.jsx
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
 import style from "../styles/homePage.module.css";
-import {useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Customizedbutton from "../customButton/customButton";
 import marketpicture from "../pictures/market_picture.jpeg";
 import cartlogo from "../pictures/cart.png";
 import cloth from "../pictures/cloth_display.jpeg";
 import chair from "../pictures/chair_display.jpeg";
-import advert from "../pictures/advert.jpeg"
-import welcomeWoman from "../pictures/welcomeWoman.jpg"; 
+import advert from "../pictures/new_boutique_image.jpg";
+import welcomeWoman from "../pictures/welcomeWoman.jpg";
 import welcome from "../pictures/welcome.jpeg";
 import first_div_1 from "../product_picture/first_div_1.jpeg";
 import first_div_2 from "../product_picture/first_div_2.jpeg";
-import first_div_3 from "../product_picture/first_div_3.jpeg"
+import first_div_3 from "../product_picture/first_div_3.jpeg";
 import first_div_4 from "../product_picture/first_div_4.jpeg";
 import second_div_1 from "../product_picture/second_div_1.jpeg";
 import second_div_2 from "../product_picture/second_div_2.jpeg";
@@ -21,8 +21,7 @@ import third_div_1 from "../product_picture/third_div_1.jpeg";
 import third_div_2 from "../product_picture/third_div_2.jpeg";
 import third_div_3 from "../product_picture/third_div_3.jpeg";
 import third_div_4 from "../product_picture/third_div_4.jpeg";
-// import footer_cover_div from "../pictures/footer_cover_div.jpeg";
-
+import Screenshot from "../product_picture/Screenshot.png";
 
 const HomePage = () => {
   const [cartCount, setCartCount] = useState(0);
@@ -34,32 +33,97 @@ const HomePage = () => {
     setCartCount((prevCount) => (prevCount > 0 ? prevCount - 1 : 0));
   };
 
-  // this is route method
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate("/Login");
+  };
 
-    const handleNavigate = () => {
-      navigate("/Login"); // Navigate to the home page route
-    };
-    // const handleNavigateToHomePage = () =>{
-    //   navigate("/homePage")
-    // }
-
+  // Array of product data
+  const products = [
+    {
+      image: first_div_1,
+      price: "#5,000",
+      name: "Men jean trouser",
+      size: "M",
+    },
+    {
+      image: first_div_2,
+      price: "#5,000",
+      name: "Men jean trouser",
+      size: "L",
+    },
+    {
+      image: first_div_3,
+      price: "#5,000",
+      name: "Women jean trouser",
+      size: "S",
+    },
+    {
+      image: first_div_4,
+      price: "#5,000",
+      name: "Women jean trouser",
+      size: "M",
+    },
+    {
+      image: second_div_1,
+      price: "#15,450",
+      name: "Men sneakers",
+      size: "42",
+    },
+    {
+      image: second_div_2,
+      price: "#15,500",
+      name: "Men sneakers",
+      size: "43",
+    },
+    {
+      image: second_div_3,
+      price: "#15,000",
+      name: "Men sneakers",
+      size: "40",
+    },
+    {
+      image: second_div_4,
+      price: "#15,100",
+      name: "Men sneakers",
+      size: "44",
+    },
+    {
+      image: third_div_1,
+      price: "#3,595",
+      name: "Men boxer",
+      size: "M",
+    },
+    {
+      image: third_div_2,
+      price: "#3,595",
+      name: "Men boxer",
+      size: "M, XL L, ,XXL",
+    },
+    {
+      image: third_div_3,
+      price: "#3,595",
+      name: "Men boxer",
+      size: "M, XL L, ,XXL",
+    },
+    {
+      image: third_div_4,
+      price: "#3,595",
+      name: "Men boxer",
+      size: "M, XL L, ,XXL",
+    },
+  ];
 
   return (
     <div>
       <div className={style.navBar_body}>
-        {/* <div className={style.company_name}> */}
-        <h1 className={style.name}>
-          <i>@whales</i>
-        </h1>
-        {/* </div> */}
-
+        <img src={Screenshot} alt="Screenshot.png" width={100} height={35} />
         <div className={style.search_bar}>
           <div className={style.navbar_search_div}>
             <input
               className={style.nav_bar_search_input}
               type="text"
-              placeholder="Search product and cartegory"
+              placeholder="Search product here"
             />
           </div>
           <div className={style.search_bar_button}>
@@ -72,21 +136,16 @@ const HomePage = () => {
             <img src={cartlogo} alt="Cart.png" />
             <span className={style.cartCount}>{cartCount}</span>
           </div>
-
-          {/* <div className={style.navButton}> */}
           <div>
             <Customizedbutton
               style={style.Loginbutton}
               type="submit"
               textContent="Login"
               onClick={handleNavigate}
-              route="/"
-              // className={style}
             />
           </div>
-
           <div>
-            <Customizedbutton
+             <Customizedbutton
               style={style.Logoutbutton}
               type="submit"
               textContent="Logout"
@@ -95,215 +154,42 @@ const HomePage = () => {
               // className={style}
             />
           </div>
-          {/* </div> */}
         </div>
       </div>
 
-      {/* maquewe display */}
-      <div className={style.second_navbar_ProductFlyer}>
-        <div className={style.marquee}>
-          <h1 className={style.modern}>
-            <i>WE HAVE MODERN DESIGN WITH HIGH QUALITY MATERIALS,</i>{" "}
-          </h1>
-          <h1 className={style.modern2}>
-            <i>IT'S ALL IN AFFORDABLE PRICE</i>
-          </h1>
-        </div>
-      </div>
-
-      {/* advert display */}
       <div className={style.advert}>
-        <img src={advert} height={200} alt="" width={1310} />
+        <img src={advert} height={200} alt="" width={1000} />
       </div>
 
+      {/* Display Products Dynamically */}
       <div className={style.firstDivDisplay}>
-        <div className={style.first_advert_color}>
-          <img
-            className={style.first_div_1}
-            src={first_div_1}
-            width={200}
-            height={250}
-            alt=""
-          />
-          <h2>#5,000</h2>
-          <h5>Men jean trouser</h5>
-          <button className={style.add_to_cart_button}>
-            onClick={handleAddToCart}
-            <b>add to cart</b>
-          </button>
-        </div>
-        <div className={style.first_advert_color}>
-          <img
-            className={style.first_div_2}
-            src={first_div_2}
-            width={180}
-            height={250}
-            alt=""
-          />
-          <h2>#5,000</h2>
-          <h5>Men jean trouser</h5>
-          <button
-            className={style.add_to_cart_button}
-            onClick={handleAddToCart}
-          >
-            <b>add to cart</b>
-          </button>
-        </div>
-        <div className={style.first_advert_color}>
-          <img
-            className={style.first_div_3}
-            src={first_div_3}
-            width={170}
-            height={250}
-            alt=""
-          />
-          <h2>#5,000</h2>
-          <h5>Women jean trouser</h5>
-          <button
-            className={style.add_to_cart_button}
-            onClick={handleAddToCart}
-          >
-            <b>add to cart</b>
-          </button>
-        </div>
-        <div className={style.first_advert_color}>
-          <img
-            className={style.first_div_4}
-            src={first_div_4}
-            width={170}
-            height={250}
-            alt=""
-          />
-          <h2>#5,000</h2>
-          <h5>Women jean trouser</h5>
-          <button
-            className={style.add_to_cart_button}
-            onClick={handleAddToCart}
-          >
-            <b>add to cart</b>
-          </button>
-        </div>
+        {products.map((product, index) => (
+          <div key={index} className={style.first_advert_color}>
+            <img
+              src={product.image}
+              width={170}
+              height={250}
+              alt={product.name}
+            />
+            <h2>{product.price}</h2>
+            <h5>{product.name}</h5>
+            <h6>Size: {product.size}</h6>
+            <button
+              className={style.add_to_cart_button}
+              onClick={handleAddToCart}
+            >
+              <b>add to cart</b>
+            </button>
+          </div>
+        ))}
       </div>
 
-      {/* view all div */}
-
+      {/* View All Button */}
       <div className={style.view_all_button_div}>
         <button className={style.view_all_button}> View All</button>
       </div>
 
-      {/* first advert div */}
-
-      <div className={style.second_navbar}>
-        <div>
-          <img src={second_div_1} height={250} alt="" width={170} />
-          <h2>#15,450</h2>
-          <h5>Men sneakers</h5>
-          <button
-            className={style.add_to_cart_button}
-            onClick={handleAddToCart}
-          >
-            <b>add to cart</b>
-          </button>
-        </div>
-        <div>
-          <img src={second_div_2} height={250} width={170} alt="" />
-          <h2>#15,500</h2>
-          <h5>Men sneakers</h5>
-          <button
-            className={style.add_to_cart_button}
-            onClick={handleAddToCart}
-          >
-            <b>add to cart</b>
-          </button>
-        </div>
-        <div>
-          <img src={second_div_3} height={250} alt="" width={170} />
-          <h2>#15,000</h2>
-          <h5>Men sneakers</h5>
-          <button
-            className={style.add_to_cart_button}
-            onClick={handleAddToCart}
-          >
-            <b>add to cart</b>
-          </button>
-        </div>
-        <div>
-          <img src={second_div_4} height={250} width={170} alt="" />
-          <h2>#15,100</h2>
-          <h5>Men sneakers</h5>
-          <button
-            className={style.add_to_cart_button}
-            onClick={handleAddToCart}
-          >
-            <b>add to cart</b>
-          </button>
-        </div>
-      </div>
-
-      {/*second view all div */}
-
-      <div className={style.view_all_button_div}>
-        <button className={style.view_all_button}> View All</button>
-      </div>
-
-      {/* third div */}
-
-      <div className={style.third_navbar}>
-        <div>
-          <img src={third_div_1} height={250} alt="" width={170} />
-          <h2>#3,595</h2>
-          <h5>Men boxer</h5>
-          <button
-            className={style.add_to_cart_button}
-            onClick={handleAddToCart}
-          >
-            <b>add to cart</b>
-          </button>
-        </div>
-        <div>
-          <img src={third_div_2} height={250} width={170} alt="" />
-          <h2>#3,595</h2>
-          <h5>Men boxer</h5>
-          <button
-            className={style.add_to_cart_button}
-            onClick={handleAddToCart}
-          >
-            <b>add to cart</b>
-          </button>
-        </div>
-        <div>
-          <img src={third_div_3} height={250} alt="" width={170} />
-          <h2>#3,595</h2>
-          <h5>Men boxer</h5>
-          <button
-            className={style.add_to_cart_button}
-            onClick={handleAddToCart}
-          >
-            <b>add to cart</b>
-          </button>
-        </div>
-        <div>
-          <img src={third_div_4} height={250} width={170} alt="" />
-          <h2>#3,595</h2>
-          <h5>Men boxer</h5>
-          <button
-            className={style.add_to_cart_button}
-            onClick={handleAddToCart}
-          >
-            <b>add to cart</b>
-          </button>
-        </div>
-      </div>
-
-      {/*second view all div */}
-
-      <div className={style.view_all_button_div}>
-        <button className={style.view_all_button}> View All</button>
-      </div>
-
-      {/* fourt div */}
-
-      <div className={style.fourth_navbar}>
+            <div className={style.fourth_navbar}>
         <div className={style.become_a_seller}>
           <div className={style.testimonials}>
             <h4>
@@ -343,7 +229,7 @@ const HomePage = () => {
         </div>
       </div>
 
-      {/* fifth div */}
+
       <div className={style.fifth_navbar}>
         <div className={style.footer_cover_div}>
           {/* <div className={style.first_footer_div}>
@@ -375,10 +261,8 @@ const HomePage = () => {
         </div>
       </div>
 
-      <div>
-        <button onClick={handleAddToCart}>Add Item</button>
-        <button onClick={handleRemoveFromCart}>Remove Item</button>
-      </div>
+      {/* Other Divs */}
+      {/* Repeat similar approach for other categories */}
     </div>
   );
 };
